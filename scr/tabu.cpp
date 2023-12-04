1cca94e197eb54b6fd2796eea33bf8b6833c44b9
bkwasny1 <kwasnyblazej1@gmail.com>
2023-12-04 20:59:27 +0100

    Koncepcja sąsiedztwa

--- scr/tabu.cpp
+++ scr/tabu.cpp
@@ -1,8 +1,25 @@
 #include "tabu.hpp"
+#include "structures.hpp"
 
-void NeighbourSelect(){
 
+/* Sasiedztwa  (przez dostępnych pacjentów rozumie sie ze nie ma ich w liscie tabu)
+1 - wymiana zawsze pierwszych wolnych pacjentow miedzy looswymi karetkami
+2 - wymiana losowych dostępnych pacjentów między losowymi karetkami
+3 - losowa zamiana każdego pacjenta między karetkami
+4 - wzięcie najlepszego dostępnego rozwiązania
+5 - co n-ta iteracje wziecie najgorszego rozwiazania
+*/
+
+void NeighbourSelect(int Tabu___List, std::vector<Ambulance*> solutions){
+    srand(time(NULL));
+    int ambulance_idx1 = rand() % AMBULANCE_NUMBER;
+    int ambulacne_idx2 = rand() % AMBULANCE_NUMBER;
+    while(ambulacne_idx2 == ambulance_idx1){
+        ambulacne_idx2 = rand() % AMBULANCE_NUMBER;
+    }
 }
+
+
 
 void UpdateTabu(){
 
