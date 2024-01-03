166ffcf29704cd8dab820ee40ce32e2d0bd9d85d
bkwasny1 <kwasnyblazej1@gmail.com>
2024-01-03 21:47:26 +0100

    Implementacja tabu listy

--- scr/tabu.cpp
+++ scr/tabu.cpp
@@ -1,6 +1,28 @@
 #include "tabu.hpp"
 #include "structures.hpp"
 
+//Tabulist
+
+void TabuList::insert_t(std::map<Ambulance, int> pair1, std::map<Ambulance, int> pair2){
+    if (size_ >= save_index){
+        tabu_l.push_back(pair1);
+        tabu_l.push_back(pair2);
+        save_index = save_index + 2;
+    }
+    else{
+        int index = save_index % size_;
+        tabu_l[index] = pair1;
+        tabu_l[index + 1] = pair2;
+        save_index = save_index + 2;
+    }
+};
+
+int TabuList::check_if_in_tabu(int) {
+
+}
+
+
+
 
 /* Sasiedztwa  (przez dostępnych pacjentów rozumie sie ze nie ma ich w liscie tabu)
 1 - wymiana zawsze pierwszych wolnych pacjentow miedzy looswymi karetkami
@@ -9,7 +31,11 @@
 4 - wzięcie najlepszego dostępnego rozwiązania
 5 - co n-ta iteracje wziecie najgorszego rozwiazania
 */
+
 void swap(Ambulance &amb1, Ambulance &amb2, int patient1_idx, int patient2_idx){
+    std::map<Ambulance, int> pair1 {{amb1, patient1_idx}};
+    std::map<Ambulance, int> pair2 {{amb2, patient2_idx}};
+
     std::vector<Patient*> order1 = *amb1.get_order();
     std::vector<Patient*> order2 = *amb2.get_order();
     Patient* patient_temp = order1[patient1_idx];
