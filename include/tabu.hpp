e799b01916cdc280c7b429ed9c44154f89849232
bkwasny1 <kwasnyblazej1@gmail.com>
2024-01-08 21:55:09 +0100

    Implementacja funkcji dla algorytmu tabu
    Dodanie funkcji + zmiany w strukturachj

--- include/tabu.hpp
+++ include/tabu.hpp
@@ -9,15 +9,19 @@
 #include <utility>
 #include <vector>
 
-//pewnie jako tablica o ograniczonej pojemnosci
+//wektor, ktory ma swoja dlugosc, po przekroczeniu dlugosci zaczyna nadpisywac
+//elementy zaczynajac od poczatku, wektor ma dlugosc dwa razy wieksza niz podana
+//w tabu, przy jednej iteracji dodaje dwie pary do listy:
+// {karetka1, index1_przeniesionego_pacjenta}, {karetka2, index2_przeniesionego_pacjenta}
+
 class TabuList{
 public:
     TabuList(int size): size_(size * 2){}
 
-//zmienic inta na zamiane
-    void insert_t(std::map<Ambulance, int> pair1, std::map<Ambulance, int> pair2);
+//dodaje dwie pary do listy tabu, jesli jest zapelniona dodaje od poczatku
+    void update_tabu(std::map<Ambulance, int> const &pair1, std::map<Ambulance, int> const &pair2);
 
-    int check_if_in_tabu(int);
+    int check_if_in_tabu(std::map<Ambulance, int> const &pair1, std::map<Ambulance, int> const &pair2);
 
 private:
     int size_;
@@ -25,14 +29,8 @@ private:
     int save_index = 0;
 };
 
-//na razie voidy
 
-void UpdateTabu();
 
-//tabu bedzie takiego typu jak rozwiazanie
-void TabuSearch();
-
-void ObjectiveFunction();
 
 #endif //HOSPITALS_TABU_HPP
 
--- scr/tabu.cpp
+++ scr/tabu.cpp
@@ -1,9 +1,78 @@
 #include "tabu.hpp"
 #include "structures.hpp"
+#include "queue"
+
+#define NOT_IN_TABU 0
+#define IN_TABU 1
+
+//Funkcje
+//lista wszsystkich szpitali, trzeba ja kiedys stworzyc
+std::vector<Hospital*> hospital_list;
+
+
+bool isValid(int x, int y, std::vector<std::vector<int>>& grid, std::vector<std::vector<bool>>& visited) {
+    return (x >= 0 && x < CITY_LENGTH && y >= 0 && y < CITY_HEIGTH && grid[x][y] && !visited[x][y]);
+}
+
+
+//Funkcja do znajdowania najkrotszej drogi miedzy punktami w miescie, miasto jako
+//pionowe i poziome drogi (tam jakas wartosc np 1) miejsca nieosiagalne jako 0
+int BFS(std::vector<std::vector<int>>& grid, const Point& start, const Point& end) {
+    std::vector<std::vector<bool>> visited(CITY_LENGTH, std::vector<bool>(CITY_HEIGTH, false));
+    std::queue<Point> q;
+
+    int dx[] = {0, 0, 1, -1};
+    int dy[] = {1, -1, 0, 0};
+
+    q.push(start);
+    visited[start.x][start.y] = true;
+
+    while (!q.empty()) {
+        Point current = q.front();
+        q.pop();
+
+        if (current.x == end.x && current.y == end.y) {
+            std::cout << "Znaleziono cel!" << std::endl;
+            return current.distance; // Zwraca ilość kroków do celu
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            int newX = current.x + dx[i];
+            int newY = current.y + dy[i];
+
+            if (isValid(newX, newY, grid, visited)) {
+                visited[newX][newY] = true;
+                q.push({newX, newY, current.distance + grid[newX][newY]});
+            }
+        }
+    }
+
+    std::cout << "Nie znaleziono ścieżki!" << std::endl;
+    return -1; // Brak ścieżki
+}
+
+
+//funckja szukajaca odpowiedniego szpitala dla pacjenta, zwraca ten szpital
+//najlepszy pod wzgledem czasu
+
+Hospital* get_best_hospital(Patient *pat){
+    std::vector<Hospital*> spec_hospital;
+    std::map<std::string, int> patient_injuries = pat -> get_injuries_list();
+    for (auto hospital : hospital_list){
+        std::map<std::string, int> spec = hospital->get_spec_list();
+        for (auto injurie : patient_injuries){
+            //dokonczyc
+            if (spec[injurie.first] < patient_injuries[injurie.first]){
+                break;
+            }
+        }
+        //trzeba dodac szpital jesli przejdzie cala petle
+    }
+}
 
 //Tabulist
 
-void TabuList::insert_t(std::map<Ambulance, int> pair1, std::map<Ambulance, int> pair2){
+void TabuList::update_tabu(std::map<Ambulance, int> const &pair1, std::map<Ambulance, int> const &pair2){
     if (size_ >= save_index){
         tabu_l.push_back(pair1);
         tabu_l.push_back(pair2);
@@ -17,12 +86,33 @@ void TabuList::insert_t(std::map<Ambulance, int> pair1, std::map<Ambulance, int>
     }
 };
 
-int TabuList::check_if_in_tabu(int) {
+//dla parzystych indeksow sprawdza sasiada z prawej, dla nieparzystych z lewej
+//
 
+int TabuList::check_if_in_tabu(std::map<Ambulance, int> const &pair1, std::map<Ambulance, int> const &pair2){
+    if (size_ >= save_index && save_index == 0){
+        return NOT_IN_TABU;
+    }
+    else{
+        int i = 0;
+        for(auto &pair_in_tabu : tabu_l){
+            if(pair_in_tabu == pair1){
+                if (i % 2 == 1){
+                    if (tabu_l[i - 1] == pair2) {
+                        return IN_TABU;
+                    }
+                }
+                if (i % 2 == 0){
+                    if (tabu_l[i + 1] == pair2){
+                        return IN_TABU;
+                    }
+                }
+            }
+            i++;
+        }
+        return NOT_IN_TABU;
+    }
 }
-
-
-
 
 /* Sasiedztwa  (przez dostępnych pacjentów rozumie sie ze nie ma ich w liscie tabu)
 1 - wymiana zawsze pierwszych wolnych pacjentow miedzy looswymi karetkami
@@ -42,13 +132,12 @@ void swap(Ambulance &amb1, Ambulance &amb2, int patient1_idx, int patient2_idx){
     order1[patient1_idx] = order2[patient2_idx];
     order2[patient2_idx] = patient_temp;
 
-
     // Ustaw zaktualizowane wektory pacjentów z powrotem w odpowiednich
 /*    ambulance1.get_order() = &patients1;
     ambulance2.get_order() = &patients2;*/
 }
 
-
+//dodac TabuListe w implementacji
 void NeighbourSelect(int Tabu___List, std::vector<Ambulance*> solutions){
     srand(time(NULL));
     int ambulance_idx1 = rand() % AMBULANCE_NUMBER;
@@ -60,14 +149,20 @@ void NeighbourSelect(int Tabu___List, std::vector<Ambulance*> solutions){
     swap(*solutions[ambulance_idx1], *solutions[ambulacne_idx2], 1, 1);
 }
 
-
-
-
-
-void UpdateTabu(){
-
+//funckaj celu
+double ObjectiveFunction(std::vector<Ambulance*> const &solution){
+    double cost;
+    for (auto ambulance : solution){
+        for (auto patient : *ambulance->get_order()){
+            int p_time = patient->get_time();
+            Point pat_loc = {patient->get_location_x(), patient ->get_location_y(), 0};
+            Point amb_loc = {ambulance->get_amb_location_x(), ambulance->get_amb_location_y(), 0};
+            int amb_to_pat_time = BFS(city, amb_loc, pat_loc);
+        }
+    }
 }
 
-void TabuSearch(){
+//algorytm tabu
+std::vector<Ambulance*> TabuSearch(){
 
 }
