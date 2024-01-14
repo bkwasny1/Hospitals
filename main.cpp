264760c6e2c376dc3eaff80470582112757363d5
bkwasny1 <kwasnyblazej1@gmail.com>
2024-01-14 15:25:59 +0100

    Poprawa pierwszego rozwiazania

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
-#include "window.hpp"
+//{1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1},
+//{1, 0, 2, 0, 0 ,0, 3, 0, 0, 0 ,0, 1, 0, 0, 0, 3 ,0, 0, 0, 0, 0 ,0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ,0, 1, 0, 0, 0 ,0, 1 ,2, 0, 0, 0, 0},
+//{1, 0, 1, 1, 0 ,0, 2, 0, 0, 0 ,0, 1, 0, 0, 0, 1 ,1, 0, 0, 0, 0 ,0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ,0, 2, 0, 0, 0 ,0, 1 ,1, 2, 2, 0, 0},
+//{1, 0, 0, 1, 0 ,0, 1, 0, 0, 0 ,0, 1, 0, 0, 0, 0 ,1, 2, 1, 1, 1 ,1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 1, 1 ,1, 1, 0, 0, 2 ,2, 1 ,0, 0, 1, 0, 0},
+//{1, 0, 0, 2, 2 ,1, 1, 0, 0, 0 ,0, 1, 0, 0, 0, 0 ,2, 0, 0, 0, 0 ,0, 1, 0, 1, 1, 2, 2, 2, 2, 0, 0, 0, 1, 0, 2, 0, 0 ,0, 0, 0, 2, 2 ,0, 1 ,0, 0, 2, 1, 0},
+//{1, 0, 0, 0, 2, 0, 0, 0, 0, 0 ,0, 1, 0, 0, 0, 0 ,1, 0, 0, 0, 0 ,0, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1, 0, 2, 0, 0 ,0, 0, 0, 2, 0 ,0, 1 ,0, 0, 0, 1, 0},
+//{1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1},
+//{1, 0, 0, 0, 0 ,0, 0, 0, 2, 0 ,0, 1, 0, 0, 0, 0 ,0, 1, 0, 0, 0 ,0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0 ,0, 0, 0, 0, 0 ,0, 1 ,0, 2, 0, 0, 0},
+//{1, 0, 0, 0, 0 ,0, 0, 1, 3, 0 ,0, 1, 0, 0, 0, 0 ,0, 1, 2, 2, 1 ,1, 1, 1, 2, 1, 2, 3, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0 ,0, 0, 0, 0, 0 ,0, 1 ,0, 2, 2, 0, 0},
+//{1, 3, 1, 0, 0 ,0, 0, 1, 0, 0 ,0, 1, 0, 0, 0, 0 ,0, 1, 0, 0, 0 ,0, 1, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 1, 3, 2, 3, 1 ,1, 1, 2, 2, 1 ,2, 1 ,0, 0, 2, 0, 0},
+//{1, 0, 2, 0, 0 ,0, 0, 1, 1, 1 ,0, 1, 0, 0, 0, 0 ,0, 2, 0, 0, 0 ,0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 ,0, 0, 0, 0, 0 ,0, 1 ,0, 0, 1, 1, 1},
+//{1, 0, 3, 0, 0 ,0, 0, 0, 0, 2 ,0, 1, 0, 0, 0, 0 ,0, 1, 0, 0, 0 ,0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0 ,0, 0, 0, 0, 0 ,0, 1 ,0, 0, 0, 0, 1},
+//{1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1},
+//{1, 0, 2, 0, 0 ,0, 2, 0, 0, 0 ,0, 1, 0, 2, 0, 0 ,0, 1, 0, 2, 0 ,0, 1, 0, 2, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1 ,0, 0, 2, 0, 0 ,0, 1 ,0, 0, 0, 2, 0},
+//{1, 0, 1, 0, 0 ,0, 1, 0, 0, 0 ,0, 1, 0, 2, 0, 0 ,0, 2, 0, 2, 0 ,0, 1, 2, 1, 0, 0, 1, 0, 0, 0, 2, 0, 1, 0, 2, 0, 1 ,2, 1, 1, 0, 0 ,0, 1 ,0, 0, 0, 1, 0},
+//{1, 0, 1, 2, 2 ,1, 1, 0, 0, 0 ,0, 1, 2, 1, 1, 1 ,2, 1, 2, 1, 1 ,1, 1, 0, 1, 0, 0, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2 ,0, 0, 2, 0, 0 ,0, 1 ,0, 0, 0, 1, 0},
+//{1, 0, 0, 0, 1 ,0, 2, 0, 0, 0 ,0, 1, 0, 1, 0, 0 ,0, 0, 0, 2, 0 ,0, 1, 0, 1, 1, 1, 2, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2 ,0, 0, 1, 1, 1 ,1, 1 ,1, 2, 2, 1, 0},
+//{1, 0, 0, 0, 1 ,0, 2, 0, 0, 0 ,0, 1, 0, 2, 0, 0 ,0, 0, 0, 1, 0 ,0, 1, 0, 2, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1 ,0, 0, 1, 0, 0 ,0, 1 ,0, 0, 0, 2, 0},
+//{1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1},
+//{1, 0, 0, 0, 0, 1, 0, 0, 0, 1 ,0, 1, 0, 1, 0, 0 ,0, 0, 0, 2, 0 ,0, 1, 0, 0, 2, 0, 0, 0, 1, 0, 1, 0, 1, 0, 2, 0, 0 ,1, 0, 0, 1, 0 ,0, 1 ,0, 2, 0, 0, 1},
+//{1, 2, 1, 2, 0, 2, 0, 0, 0, 2 ,1, 1, 0, 2, 0, 0 ,0, 0, 0, 1, 0 ,0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 2, 0, 1, 0, 2, 0, 0 ,2, 1, 1, 2, 1 ,1, 1 ,0, 2, 0, 0, 1},
+//{1, 0, 0, 1, 1, 1, 1, 2, 1, 1 ,2, 1, 1, 1, 2, 1 ,2, 1, 1, 2, 1 ,1, 1, 2, 2, 1, 0, 0, 0, 2, 1, 1, 2, 1, 1, 1, 2, 1 ,2, 0, 0, 0, 2 ,0, 1 ,1, 1, 2, 1, 2},
+//{1, 0, 0, 1, 0 ,0, 2, 0, 0, 1 ,0, 1, 0, 0, 1, 0 ,0, 0, 0, 1, 0 ,0, 1, 0, 0, 1, 1, 2, 1, 2, 0, 0, 0, 1, 0, 1, 0, 0 ,1, 0, 0, 0, 2 ,0, 1 ,0, 2, 0, 0, 2},
+//{1, 0, 0, 2, 0 ,0, 1, 0, 0, 2 ,0, 1, 0, 0, 2, 0 ,0, 0, 0, 2, 0 ,0, 1, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 1, 0, 2, 0, 0 ,2, 0, 0, 0, 1 ,0, 1 ,0, 1, 0, 0, 1},
+//{1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1 ,1, 1},
+// 1, 2, 3, 4, 5 ,6, 7, 8, 9, 10 11,12,13,14,15 16,17,18,19,20,21,22,23,24 25,26,27,28,29 30,31 32,33,34,35 36,37,38,39,40,41,42,43,44 45,46,47,48,49 50
+
+//#include "window.hpp"
 #include <iostream>
 #include <tabu.hpp>
 #include <cfloat>
@@ -7,10 +34,10 @@
 //pamietac o zmianie AMBULANCE_NUMBER
 
 //dane wejsciowe z GUI
-int max_liczba_iteracji;
-int kryterium_aspiracji;
-int dlugosc_listy_tabu;
-std::vector<int> dobor_sasiedztwa;
+int max_liczba_iteracji = 1000;
+int kryterium_aspiracji = 0;
+int dlugosc_listy_tabu = 20;
+std::vector<int> dobor_sasiedztwa = {1, 1, 1, 1};
 
 //dane wyjsciowe do GUI
 int act_liczba_iteracji = 0;
@@ -24,9 +51,10 @@ int wartosci_funkcji[10000];
 int Patient::pat_id = 0;
 int Ambulance::amb_id = 0;
 
+extern std::vector<std::vector<int>> city;
 
 int main(){
-    wczytaj_dane();
+    //wczytaj_dane();
 
     // Początkowy czas
     auto start = std::chrono::high_resolution_clock::now();
@@ -48,32 +76,80 @@ int main(){
             "Gastroenterologia"};
 
     Hospital hos_1(0,  0 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
-    Hospital hos_2(1,  1 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
-    Hospital hos_3(2,  2 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Hospital hos_2(24, 49 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Hospital hos_3(9,  7 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Hospital hos_4(9, 28 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Hospital hos_5(12, 40 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Hospital hos_6(0, 49 , specializations, std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
 
     hospital_list.push_back(&hos_1);
     hospital_list.push_back(&hos_2);
     hospital_list.push_back(&hos_3);
+    hospital_list.push_back(&hos_4);
+    hospital_list.push_back(&hos_5);
+    hospital_list.push_back(&hos_6);
 
     Ambulance ambulance_1(&hos_1);
     Ambulance ambulance_2(&hos_2);
     Ambulance ambulance_3(&hos_3);
+    Ambulance ambulance_4(&hos_1);
+    Ambulance ambulance_5(&hos_2);
+    Ambulance ambulance_6(&hos_3);
+    Ambulance ambulance_7(&hos_1);
+    Ambulance ambulance_8(&hos_2);
+
+    ambulance_list.push_back(&ambulance_1);
+    ambulance_list.push_back(&ambulance_2);
+    ambulance_list.push_back(&ambulance_3);
+    ambulance_list.push_back(&ambulance_4);
+    ambulance_list.push_back(&ambulance_5);
+    ambulance_list.push_back(&ambulance_6);
+    ambulance_list.push_back(&ambulance_7);
+    ambulance_list.push_back(&ambulance_8);
 
     Patient pat_1(0,6,3,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
     Patient pat_2(1,1,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
     Patient pat_3(0,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
     Patient pat_4(0,3,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
     Patient pat_5(1,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_6(0,6,3,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_7(1,1,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_8(0,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_9(0,3,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_10(1,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_11(0,6,3,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_12(1,1,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_13(0,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_14(0,3,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_15(1,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_16(0,6,3,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_17(1,1,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_18(0,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_19(0,3,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+    Patient pat_20(1,4,1,1, specializations,std::vector<int>{0,0,0,0,0,0,0,0,0,0,0,0,0});
+
 
     patients_list.push_back(&pat_1);
     patients_list.push_back(&pat_2);
     patients_list.push_back(&pat_3);
     patients_list.push_back(&pat_4);
     patients_list.push_back(&pat_5);
+    patients_list.push_back(&pat_6);
+    patients_list.push_back(&pat_7);
+    patients_list.push_back(&pat_8);
+    patients_list.push_back(&pat_9);
+    patients_list.push_back(&pat_10);
+    patients_list.push_back(&pat_11);
+    patients_list.push_back(&pat_12);
+    patients_list.push_back(&pat_13);
+    patients_list.push_back(&pat_14);
+    patients_list.push_back(&pat_15);
+    patients_list.push_back(&pat_16);
+    patients_list.push_back(&pat_17);
+    patients_list.push_back(&pat_18);
+    patients_list.push_back(&pat_19);
+    patients_list.push_back(&pat_20);
 
-    std::map<Ambulance*, int> neigh_to_swap;
-    neigh_to_swap.insert(std::make_pair(&ambulance_1,1));
-    neigh_to_swap.insert(std::make_pair(&ambulance_2,2));
 
     std::vector<Ambulance*> wynik = TabuSearch();
 
@@ -94,7 +170,7 @@ int main(){
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
     czas_wykonania = (int)duration.count();
 
-    zapisz_dane();
+    //zapisz_dane();
 
     return 0;
 }
